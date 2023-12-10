b6abe82b60378c134333b10d9b4430111c3f1d30
Adi-Sharma27 <150667422+Adi-Sharma27@users.noreply.github.com>
2023-12-10 15:52:37 +0530

    Add files via upload

---
+++ 68.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+ long long decimal, octal = 0;
+ int digit, place = 1;
+ printf("Aditya Sharma\n");
+ printf("Enter a decimal number: ");
+ scanf("%lld", &decimal);
+
+ while (decimal > 0) {
+ digit = decimal % 8;
+ octal += digit * place;
+ place *= 10;
+ decimal /= 8;
+ }
+ printf("Octal equivalent: %lld\n", octal);
+ return 0;
+}
---
+++ 89.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+     printf("Aditya Sharma\n");
+    printf("Enter any number to check even or odd: ");
+    scanf("%d", &num);
+
+    if(num % 2 == 0)
+    {
+        printf("Number is Even.");
+    }
+    else
+    {
+        printf("Number is Odd.");
+    }
+
+    return 0;
+}
+
